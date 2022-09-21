ea872f72748a3a9b32933a934edeb7f210d612e0
Moshfequr rahman <moshfequr9@gmail.com>
2022-09-21 18:07:25 +0600

    updated code for slider
    up

--- new-homepage.js
+++ new-homepage.js
@@ -1,1 +1,77 @@
-// code hare
+$(document).ready(function(){
+  $('.r-home-showcase-slider-arrow-left-mobile').addClass('disable');
+    $(".r-home-showcase-slider-arrow-left-mobile").on('click', function(){
+        $( ".r-home-showcase-slider-dots-mobile .w-slider-dot" ).each( function( index, element ){
+            if (index === 0 && $( this ).hasClass('w-active')){
+                $('.r-home-showcase-slider-arrow-left-mobile').addClass('disable');
+                $('.r-home-showcase-slider-arrow-right-mobile').removeClass('disable');
+            } else if (index === 1 && $( this ).hasClass('w-active')){
+                $('.r-home-showcase-slider-arrow-right-mobile').removeClass('disable');
+                $('.r-home-showcase-slider-arrow-left-mobile').removeClass('disable');
+            } else if (index === 2 && $( this ).hasClass('w-active')){
+                $('.r-home-showcase-slider-arrow-right-mobile').removeClass('disable');
+                $('.r-home-showcase-slider-arrow-left-mobile').removeClass('disable');
+            } else if (index === 3 && $( this ).hasClass('w-active')){
+                $('.r-home-showcase-slider-arrow-right-mobile').addClass('disable');
+                $('.r-home-showcase-slider-arrow-left-mobile').removeClass('disable');
+            }
+        });
+    });
+
+    $(".r-home-showcase-slider-arrow-right-mobile").on('click', function(){
+        $( ".r-home-showcase-slider-dots-mobile .w-slider-dot" ).each( function( index, element ){
+            if (index === 0 && $( this ).hasClass('w-active')){
+                $('.r-home-showcase-slider-arrow-left-mobile').addClass('disable');
+                $('.r-home-showcase-slider-arrow-right-mobile').removeClass('disable');
+            } else if (index === 1 && $( this ).hasClass('w-active')){
+                $('.r-home-showcase-slider-arrow-right-mobile').removeClass('disable');
+                $('.r-home-showcase-slider-arrow-left-mobile').removeClass('disable');
+            } else if (index === 2 && $( this ).hasClass('w-active')){
+                $('.r-home-showcase-slider-arrow-right-mobile').removeClass('disable');
+                $('.r-home-showcase-slider-arrow-left-mobile').removeClass('disable');
+            } else if (index === 3 && $( this ).hasClass('w-active')){
+                $('.r-home-showcase-slider-arrow-right-mobile').addClass('disable');
+                $('.r-home-showcase-slider-arrow-left-mobile').removeClass('disable');
+            }
+        });
+    });
+});
+
+$(document).ready(function(){
+  $('.r-home-showcase-slider-arrow-left').addClass('disable');
+    $(".r-home-showcase-slider-arrow-left").on('click', function(){
+        $( ".r-home-showcase-slider-dots .w-slider-dot" ).each( function( index, element ){
+            if (index === 0 && $( this ).hasClass('w-active')){
+                $('.r-home-showcase-slider-arrow-left').addClass('disable');
+                $('.r-home-showcase-slider-arrow-right').removeClass('disable');
+            } else if (index === 1 && $( this ).hasClass('w-active')){
+                $('.r-home-showcase-slider-arrow-right').removeClass('disable');
+                $('.r-home-showcase-slider-arrow-left').removeClass('disable');
+            } else if (index === 2 && $( this ).hasClass('w-active')){
+                $('.r-home-showcase-slider-arrow-right').removeClass('disable');
+                $('.r-home-showcase-slider-arrow-left').removeClass('disable');
+            } else if (index === 3 && $( this ).hasClass('w-active')){
+                $('.r-home-showcase-slider-arrow-right').addClass('disable');
+                $('.r-home-showcase-slider-arrow-left').removeClass('disable');
+            }
+        });
+    });
+
+    $(".r-home-showcase-slider-arrow-right").on('click', function(){
+        $( ".r-home-showcase-slider-dots .w-slider-dot" ).each( function( index, element ){
+            if (index === 0 && $( this ).hasClass('w-active')){
+                $('.r-home-showcase-slider-arrow-left').addClass('disable');
+                $('.r-home-showcase-slider-arrow-right').removeClass('disable');
+            } else if (index === 1 && $( this ).hasClass('w-active')){
+                $('.r-home-showcase-slider-arrow-right').removeClass('disable');
+                $('.r-home-showcase-slider-arrow-left').removeClass('disable');
+            } else if (index === 2 && $( this ).hasClass('w-active')){
+                $('.r-home-showcase-slider-arrow-right').addClass('disable');
+                $('.r-home-showcase-slider-arrow-left').removeClass('disable');
+            } else if (index === 3 && $( this ).hasClass('w-active')){
+                $('.r-home-showcase-slider-arrow-right').addClass('disable');
+                $('.r-home-showcase-slider-arrow-left').removeClass('disable');
+            }
+        });
+    });
+});
